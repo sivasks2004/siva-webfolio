383ddbec96e9d1b71f45184dbe40e730aeeedb6a
sivasks2004 <sivakulanthaisamy1804@gmail.com>
2025-09-17 22:39:44 +0530

    Day 3 Resume drive link upload

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -71,7 +71,7 @@ export default function AboutSection() {
             <div className="flex justify-end mt-6">
               <div className="relative group">
                 <a
-                  href="https://drive.google.com/file/d/1wQn7r2yQwKJv8vQn9vQn9vQn9vQn9vQn/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1qviWJegSrbyIYgIzN4hqTaWY43vwWDEZ/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn-primary bg-primary text-primary-foreground px-4 py-2 rounded-md font-semibold transition-all text-sm shadow-lg z-10 relative overflow-hidden group-hover:scale-105 group-active:scale-95 duration-300"
